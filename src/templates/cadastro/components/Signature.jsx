453287f440d0f731a8f28de5f72d70324b55dd9a
ramonbs <ramongiovanii12@gmail.com>
2023-05-07 17:02:25 -0300

    pushing changes

--- src/templates/cadastro/components/Signature.jsx
+++ src/templates/cadastro/components/Signature.jsx
@@ -9,7 +9,6 @@ export default function Signature({ controller }) {
       <>
         <div className={ styles[signatureClass] } />
         <div className={ styles[signatureClass] } />
-
         <div className={ styles[signatureClass] } />
       </>
     );
