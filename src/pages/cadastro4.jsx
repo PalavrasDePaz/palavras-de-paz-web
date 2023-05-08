07dccc78e8b257d7d11af20dc06bdbfd7304605e
Rafael Olivares <rafaelrolivares@gmail.com>
2023-05-08 18:36:24 -0300

    Remove arquivos não utilizados

--- src/pages/cadastro4.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CadastroQuartaTela from '../templates/cadastro/components/CadastroQuartaTela';
-
-export default function cadastro4() {
-  return (
-    <CadastroQuartaTela />
-  );
-}
