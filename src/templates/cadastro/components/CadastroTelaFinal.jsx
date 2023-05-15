2a0be78e33c040c9ffc103d6bfddcbfd536fb7dd
Rafael Olivares <rafaelrolivares@gmail.com>
2023-05-15 15:58:32 -0300

    Muda campos de boolean para array de string antes de enviar dados

--- src/templates/cadastro/components/CadastroTelaFinal.jsx
+++ src/templates/cadastro/components/CadastroTelaFinal.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/CadastroTemplate.module.css';
 
-export default function cadastroTelaFinal() {
+const filterValues = (valuesObj) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  Object.keys(valuesObj).filter((key) => valuesObj[key] !== false);
+
+export default function cadastroTelaFinal({ data } = props) {
+  // Workaround para transformar os valores dos checkbox em um array de strings
+  const { interestFutureRoles, rolesPep } = data;
+  data.interestFutureRoles = filterValues(interestFutureRoles);
+  data.rolesPep = filterValues(rolesPep);
+
+  // Também não mandamos o valor do campo de deficiencia, só qual ela é, se houver.
+  const { deficiencia, ...apiObject } = data;
+
+  console.log(apiObject);
+
   return (
     <section>
       <h1 className={ styles.formTitle }>OBRIGADO</h1>
