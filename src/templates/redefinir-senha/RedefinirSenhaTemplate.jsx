c3d6bd0c351b113c58ad6d9851ae3c27f0d6247a
Rafael Olivares <rafaelrolivares@gmail.com>
2023-05-18 12:37:19 -0300

    Adiciona página de recuperação de senha

---
+++ src/templates/redefinir-senha/RedefinirSenhaTemplate.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import React from 'react';
+import Logo from '../../../public/static/images/logo.svg';
+import LoginForm from './components/Form';
+import styles from './styles/RedefinirSenha.style.module.css';
+
+function RedefinirSenhaTemplate() {
+  return (
+    <section className={ styles.loginSection }>
+
+      <Image
+        src={ Logo }
+        alt="logo com a frase Palavras de Paz,
+        programa de educação para a paz,
+        escrito em verde"
+        width="333px"
+        height="150px"
+      />
+
+      <section className={ styles.loginSectionForm }>
+        <LoginForm />
+      </section>
+
+    </section>
+  );
+}
+
+export default RedefinirSenhaTemplate;
