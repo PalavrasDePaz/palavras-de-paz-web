93b5bb64c0071d1a4bf4a153bc8bb86286dfe341
Rafael Olivares <rafaelrolivares@gmail.com>
2023-05-23 18:45:43 -0300

    Envia cadastro e faz login

---
+++ src/templates/login/components/BackButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/LoginForm.module.css';
+
+export default function BackButton() {
+  return (
+    <Link href="/" className={ styles.loginFormButtonBack }>
+      <button className={ styles.loginFormButtonBack }>
+        Voltar para a página inicial
+      </button>
+    </Link>
+  );
+}
