2f3f8a551a3681dc2e8766d36997fc547c1948cd
Rafael Olivares <rafaelrolivares@gmail.com>
2023-05-26 12:39:18 -0300

    Deixar ESLint feliz

--- src/templates/login/components/LoginInputs.jsx
+++ src/templates/login/components/LoginInputs.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import axios from "axios";
-import { useRouter } from "next/router";
-import styles from "../styles/LoginForm.module.css";
-import { API } from "../../../constants";
-import LoadingSpinner from "../../../components/loadingSpinner/LoadingSpinner";
-import { REQUIRED_FIELD } from "../../cadastro/components/constants";
-import BackButton from "./BackButton";
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import styles from '../styles/LoginForm.module.css';
+import { API } from '../../../constants';
+import LoadingSpinner from '../../../components/loadingSpinner/LoadingSpinner';
+import { REQUIRED_FIELD } from '../../cadastro/components/constants';
+import BackButton from './BackButton';
 
 const MIN_PASSWORD_LENGTH = 6;
 
@@ -40,8 +40,8 @@ function LoginForm() {
     resolver: yupResolver(passwordForgotten ? schemaResetPass : schemaLogin),
   });
 
-  const loginAddress = `${API}/volunteers/login`;
-  const resetPassAddress = `${API}/volunteers/password-email`;
+  const loginAddress = `${ API }/volunteers/login`;
+  const resetPassAddress = `${ API }/volunteers/password-email`;
 
   const postAddress = passwordForgotten ? resetPassAddress : loginAddress;
 
@@ -55,15 +55,14 @@ function LoginForm() {
     axios
       .post(postAddress, data)
       // TODO: a resposta de login será um token, que vamos levar para a área de trabalho.
-      .then(() => (passwordForgotten ? updateComponentAfterMail() : push("/")))
+      .then(() => (passwordForgotten ? updateComponentAfterMail() : push('/')))
       .catch((error) => {
         setApiError(error);
         console.log(error);
       });
 
   // No caso de senha perdida, mandamos só o email
-  const cleanData = (data) =>
-    passwordForgotten ? { email: data.email } : data;
+  const cleanData = (data) => (passwordForgotten ? { email: data.email } : data);
 
   const onSubmit = (data) => {
     setIsSending(true);
@@ -77,16 +76,16 @@ function LoginForm() {
   }
 
   const emailFieldString = passwordForgotten ? (
-    "Informe seu e-mail para enviarmos um link de redefinição de senha"
+    'Informe seu e-mail para enviarmos um link de redefinição de senha'
   ) : (
     <b>E-mail</b>
   );
-  const buttonString = passwordForgotten ? "Enviar e-mail" : "Entrar";
+  const buttonString = passwordForgotten ? 'Enviar e-mail' : 'Entrar';
 
   if (apiError) {
     return (
       <>
-        <p className={styles.formParagraph} style={{ color: "red" }}>
+        <p className={ styles.formParagraph } style={ { color: 'red' } }>
           Ocorreu um erro inesperado. Tente novamente mais tarde
         </p>
         <BackButton />
@@ -102,7 +101,7 @@ function LoginForm() {
     // Apenas com a opção de email enviado porque em caso de login correto vai haver o redirect.
     return (
       <>
-        <p className={styles.formParagraph}>
+        <p className={ styles.formParagraph }>
           O e-mail de recuperação de senha foi enviado!
         </p>
         <BackButton />
@@ -111,30 +110,30 @@ function LoginForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.loginFormSection}>
+    <form onSubmit={ handleSubmit(onSubmit) } className={ styles.loginFormSection }>
       {!passwordForgotten && (
-        <h1 className={styles.loginFormSectionTitle}>Faça seu login</h1>
+        <h1 className={ styles.loginFormSectionTitle }>Faça seu login</h1>
       )}
 
-      <div className={styles.loginFormSectionInputContainer}>
-        <label className={styles.loginFormSectionInputLabel} htmlFor="email">
+      <div className={ styles.loginFormSectionInputContainer }>
+        <label className={ styles.loginFormSectionInputLabel } htmlFor="email">
           {emailFieldString}
         </label>
 
         <input
           placeholder="nome@palavrasdepaz.com.br"
           type="email"
-          className={styles.loginFormSectionInputEmail}
-          {...register("email")}
+          className={ styles.loginFormSectionInputEmail }
+          { ...register('email') }
         />
         {errors.email && (
-          <p className={styles.inputError}>{errors.email.message}</p>
+          <p className={ styles.inputError }>{errors.email.message}</p>
         )}
       </div>
       {!passwordForgotten && (
-        <div className={styles.loginFormSectionInputContainer}>
+        <div className={ styles.loginFormSectionInputContainer }>
           <label
-            className={styles.loginFormSectionInputLabel}
+            className={ styles.loginFormSectionInputLabel }
             htmlFor="password"
           >
             <b>Senha</b>
@@ -142,35 +141,35 @@ function LoginForm() {
 
           <input
             placeholder="Digite sua senha"
-            className={styles.loginFormSectionInputPassword}
-            type={isPasswordVisible ? "text" : "password"}
-            {...register("password")}
+            className={ styles.loginFormSectionInputPassword }
+            type={ isPasswordVisible ? 'text' : 'password' }
+            { ...register('password') }
           />
 
           <button
-            className={styles.loginFormSectionInputPasswordVisibility}
+            className={ styles.loginFormSectionInputPasswordVisibility }
             type="button"
-            onClick={handlePasswordVisibility}
+            onClick={ handlePasswordVisibility }
           >
             {isPasswordVisible ? <FaEye /> : <FaEyeSlash />}
           </button>
           {errors.password && (
-            <p className={styles.inputError}>{errors.password.message}</p>
+            <p className={ styles.inputError }>{errors.password.message}</p>
           )}
         </div>
       )}
       {!passwordForgotten && (
-        <section className={styles.loginFormSectionButtonsContainer}>
+        <section className={ styles.loginFormSectionButtonsContainer }>
           <button
-            className={styles.loginFormSectionButtons}
-            onClick={() => setPasswordForgotten(true)}
+            className={ styles.loginFormSectionButtons }
+            onClick={ () => setPasswordForgotten(true) }
           >
             Esqueceu a senha?
           </button>
         </section>
       )}
 
-      <button type="submit" className={styles.loginFormButtonEnter}>
+      <button type="submit" className={ styles.loginFormButtonEnter }>
         {buttonString}
       </button>
       <BackButton />
---
+++ src/templates/redefinir-senha/components/Form.jsx
@@ -0,0 +1,147 @@
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import styles from '../styles/RedefinirSenha.module.css';
+import {
+  REQUIRED_FIELD,
+  PASS_MIN,
+  PASS_MISMATCH,
+} from '../../cadastro/components/constants';
+import ErrorMessage from '../../../components/forms/ErrorMessage';
+import { API } from '../../../constants';
+import BackButton from '../../login/components/BackButton';
+import LoadingSpinner from '../../../components/loadingSpinner/LoadingSpinner';
+
+const MIN_PASSWORD_LENGTH = 6;
+
+const schema = yup.object().shape({
+  password: yup
+    .string()
+    .required(REQUIRED_FIELD)
+    .min(MIN_PASSWORD_LENGTH, PASS_MIN),
+  passConfirmation: yup
+    .string()
+    .required(REQUIRED_FIELD)
+    .min(MIN_PASSWORD_LENGTH, PASS_MIN)
+    .equals([yup.ref('password')], PASS_MISMATCH),
+});
+
+function Form() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isPassConfVisible, setIsPassConfVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+  const [apiError, setApiError] = useState();
+
+  const router = useRouter();
+  const mailHash = router.query.hash;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const route = `${ API }/volunteers/password`;
+
+  const onSubmit = ({ password }) => {
+    setIsLoading(true);
+    axios
+      .patch(route, { password, hashEmail: mailHash })
+      .then(() => {
+        setIsLoading(false);
+        setIsSent(true);
+        // eslint-disable-next-line no-magic-numbers
+        setTimeout(() => router.push('/login'), 3000);
+      })
+      .catch((error) => {
+        setApiError(error);
+        console.log(error);
+      });
+  };
+
+  if (apiError) {
+    return (
+      <>
+        <p className={ styles.formParagraph } style={ { color: 'red' } }>
+          Ocorreu um erro inesperado. Tente novamente mais tarde
+        </p>
+        <BackButton />
+      </>
+    );
+  }
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (isSent) {
+    return (
+      <p className={ styles.formParagraph }>
+        Nova senha criada! Redirecionando para login...
+      </p>
+    );
+  }
+
+  return (
+    <form onSubmit={ handleSubmit(onSubmit) } className={ styles.loginFormSection }>
+      <h1 className={ styles.loginFormSectionTitle }>Crie sua nova senha</h1>
+      <div className={ styles.loginFormSectionInputContainer }>
+        <label className={ styles.loginFormSectionInputLabel } htmlFor="password">
+          <b>Senha</b>
+        </label>
+        <input
+          placeholder="Digite sua senha"
+          className={ styles.loginFormSectionInputPassword }
+          type={ isPasswordVisible ? 'text' : 'password' }
+          { ...register('password') }
+        />
+        <button
+          className={ styles.loginFormSectionInputPasswordVisibility }
+          type="button"
+          onClick={ () => setIsPasswordVisible((_isVisible) => !_isVisible) }
+        >
+          {isPasswordVisible ? <FaEye /> : <FaEyeSlash />}
+        </button>
+        <ErrorMessage showError={ errors.password } style={ styles.inputError } />
+      </div>
+      <div className={ styles.loginFormSectionInputContainer }>
+        <label className={ styles.loginFormSectionInputLabel } htmlFor="password">
+          <b>Confirmação</b>
+        </label>
+        <input
+          placeholder="Confirme sua senha"
+          className={ styles.loginFormSectionInputPassword }
+          type={ isPassConfVisible ? 'text' : 'password' }
+          { ...register('passConfirmation') }
+        />
+        <button
+          className={ styles.loginFormSectionInputPasswordVisibility }
+          type="button"
+          onClick={ () => setIsPassConfVisible((_isVisible) => !_isVisible) }
+        >
+          {isPasswordVisible ? <FaEye /> : <FaEyeSlash />}
+        </button>
+        <ErrorMessage
+          showError={ errors.passConfirmation }
+          style={ styles.inputError }
+        />
+      </div>
+      <button className={ styles.loginFormButtonEnter }>Enviar</button>
+      <Link href="/" className={ styles.loginFormButtonBack }>
+        <button className={ styles.loginFormButtonBack }>
+          Voltar para a página inicial
+        </button>
+      </Link>
+    </form>
+  );
+}
+
+export default Form;
