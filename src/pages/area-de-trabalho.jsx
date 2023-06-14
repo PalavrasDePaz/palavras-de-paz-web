badaa94b630e9f38c74d91a7cf55b1630cbf0496
Rafael Olivares <rafaelrolivares@gmail.com>
2023-06-14 12:32:15 -0300

    Change file name case

--- src/pages/area-de-trabalho.jsx
+++ src/pages/area-de-trabalho.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import AreaDeTrabalhoTemplate from '../templates/area-de-trabalho/areaDeTrabalhoTemplate';
+import AreaDeTrabalhoTemplate from '../templates/area-de-trabalho/AreaDeTrabalhoTemplate';
 
 export default function areaDeTrabalho() {
   return <AreaDeTrabalhoTemplate />;
