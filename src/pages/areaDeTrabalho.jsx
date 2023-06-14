40c7acaa32195865ea8e7eab8b87e1072e921586
WellingtonMarques96 <well06marques@gmail.com>
2023-06-14 11:46:50 -0300

    corpo e estilização área de trabalho

---
+++ src/pages/areaDeTrabalho.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import AreaDeTrabalhoTemplate from '../templates/area-de-trabalho/areaDeTrabalhoTemplate';
+
+export default function areaDeTrabalho() {
+  return <AreaDeTrabalhoTemplate />;
+}
