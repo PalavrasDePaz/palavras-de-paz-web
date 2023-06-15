e64582d329ecc0f15669c92388d4300505b98095
Rafael Olivares <rafaelrolivares@gmail.com>
2023-06-15 12:06:11 -0300

    Test

--- src/templates/presenca/schema.ts
+++ src/templates/presenca/schema.ts
@@ -1,11 +1,11 @@
 import * as yup from 'yup';
 
 export const schema = yup.object().shape({
-  subject: yup.string(),
-  enoughTime: yup.string(),
-  aproveitamento: yup.string(),
-  improvements: yup.string(),
-  applyInLife: yup.string(),
-  learnedToday: yup.string(),
-  freeform: yup.string(),
+  subject: yup.string().nullable(),
+  enoughTime: yup.string().nullable(),
+  aproveitamento: yup.string().nullable(),
+  improvements: yup.string().nullable(),
+  applyInLife: yup.string().nullable(),
+  learnedToday: yup.string().nullable(),
+  freeform: yup.string().nullable(),
 });
