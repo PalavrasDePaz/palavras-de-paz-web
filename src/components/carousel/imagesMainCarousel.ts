eaef9a9f93a43af71c7056ebbdd1e828ef541704
rafael olivares <olrafael@mailfence.com>
2023-06-16 10:47:01 -0300

    Adiciona novas imagens à home (#63)

---
+++ src/components/carousel/imagesMainCarousel.ts
@@ -0,0 +1,35 @@
+import img01 from '../../../public/static/images/img01_moradores.jpeg';
+import img02 from '../../../public/static/images/img02_refugees.jpeg';
+import img03 from '../../../public/static/images/img03_refugees.jpeg';
+import img04 from '../../../public/static/images/img04_imigrantes.jpeg';
+import img05 from '../../../public/static/images/img05_imigrantes.jpeg';
+
+const imagesMainCarousel = [
+  {
+    id: 1,
+    image: img01,
+    text: 'Projeto com Moradores de Rua',
+  },
+  {
+    id: 2,
+    image: img02,
+    text: 'Projeto com Refugiados',
+  },
+  {
+    id: 3,
+    image: img03,
+    text: 'Projeto com Refugiados',
+  },
+  {
+    id: 4,
+    image: img04,
+    text: 'Projeto com Imigrantes',
+  },
+  {
+    id: 5,
+    image: img05,
+    text: 'Projeto com Imigrantes',
+  },
+];
+
+export default imagesMainCarousel;
