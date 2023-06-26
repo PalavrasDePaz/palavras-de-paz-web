b2d6d3bf26358a8066ac081d4b4a799081c4a76a
Rafael Olivares <rafaelrolivares@gmail.com>
2023-06-26 17:12:36 -0300

    Adiciona novas fotos à home

--- src/components/carousel/imagesMainCarousel.ts
+++ src/components/carousel/imagesMainCarousel.ts
@@ -1,39 +1,25 @@
 import img01 from "../../../public/static/images/img01_moradores.jpeg";
 import img02 from "../../../public/static/images/img02_refugees.jpeg";
 import img03 from "../../../public/static/images/img03_refugees.jpeg";
-import img04 from "../../../public/static/images/img04_imigrantes.jpeg";
-import img05 from "../../../public/static/images/img05_imigrantes.jpeg";
+/* import img04 from "../../../public/static/images/img04_imigrantes.jpeg";
+import img05 from "../../../public/static/images/img05_imigrantes.jpeg"; */
 import img06 from "../../../public/static/images/img06_inmates.jpeg";
 import img07 from "../../../public/static/images/img07_inmates.jpeg";
+import img08 from "../../../public/static/images/img08_violencia.jpeg";
+import img09 from "../../../public/static/images/img09_imigrantes.jpeg";
+import img10 from "../../../public/static/images/img10_imigrantes.jpeg";
 
 const imagesMainCarousel = [
-  {
-    id: 1,
-    image: img01,
-    text: "Projeto com Moradores de Rua",
-  },
-  {
-    id: 2,
-    image: img02,
-    text: "Projeto com Refugiados",
-  },
-  {
-    id: 3,
-    image: img03,
-    text: "Projeto com Refugiados",
-  },
-  {
-    id: 4,
-    image: img04,
-    text: "Projeto com Imigrantes",
-  },
-  {
-    id: 5,
-    image: img05,
-    text: "Projeto com Imigrantes",
-  },
+  { id: 1, image: img01, text: "Projeto com Moradores de Rua" },
+  { id: 2, image: img02, text: "Projeto com Refugiados" },
+  { id: 3, image: img03, text: "Projeto com Refugiados" },
+  /*  { id: 4, image: img04, text: "Projeto com Imigrantes" },
+  { id: 5, image: img05, text: "Projeto com Imigrantes" }, */
+  { id: 9, image: img09, text: "Projeto com Imigrantes" },
+  { id: 10, image: img10, text: "Projeto com Imigrantes" },
   { id: 6, image: img06, text: "Projeto com População Carcerária" },
   { id: 7, image: img07, text: "Projeto com População Carcerária" },
+  { id: 8, image: img08, text: "Project com Vítimas de Violência" },
 ];
 
 export default imagesMainCarousel;
