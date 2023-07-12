e042fa7bff4b20970920e781f7b84361604428c7
Rafael Olivares <rafaelrolivares@gmail.com>
2023-07-12 16:04:11 -0300

    fix

--- src/hooks/useGetNotebooks.ts
+++ src/hooks/useGetNotebooks.ts
@@ -1,17 +1,11 @@
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import { api } from "../api";
-
-type Workshops = {
-  idAttend: number;
-  submissionDate: string;
-  workshopSubject: string;
-};
 
 const getNotebooks = async (volunteerId: number) =>
   (await api.get(`/notebooks/available/${volunteerId}`)).data;
 
-const useGetNotebooks = (volunteerId: number): UseQueryResult<Workshops[]> =>
+const useGetNotebooks = (volunteerId: number) =>
   useQuery(["notebooks", volunteerId], () => getNotebooks(volunteerId));
 
 export default useGetNotebooks;
