b7503b695f452ee751c4955f81de3d35d10fcfb0
rafael olivares <olrafael@mailfence.com>
2023-07-20 14:33:48 -0300

    Adiciona relatório semestral (#87)

---
+++ src/pages/doacoes/relatorio-semestral-2023-01.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PdfEmbed from "../../templates/publicacoes/pdfEmbed/PdfEmbed";
+import { Article } from "../../templates/publicacoes/pdfEmbed/types";
+
+const impactoFile: Article = {
+  title: "Relatório do Primeiro Semestre de 2023",
+  description: "Transformando vidas, construindo um futuro melhor.",
+  url: "/static/assets/RelatorioSemestral2023_01.pdf",
+  image: "https://www.palavrasdepaz.org/static/images/livro.jpg",
+};
+
+const Relatorio = () => <PdfEmbed article={impactoFile} />;
+
+export default Relatorio;
