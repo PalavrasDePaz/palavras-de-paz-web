0d9cf1c35a64ed7227ac49854236f993f842e077
Rafael Olivares <rafaelrolivares@gmail.com>
2023-07-20 14:31:46 -0300

    fix

--- src/pages/doacoes/relatorio-semestral-2023-01.tsx
+++ src/pages/doacoes/relatorio-semestral-2023-01.tsx
@@ -5,9 +5,7 @@ import { Article } from "../../templates/publicacoes/pdfEmbed/types";
 
 const impactoFile: Article = {
   title: "Relatório do Primeiro Semestre de 2023",
-  description:
-    // eslint-disable-next-line max-len
-    "Transformando vidas, construindo um futuro melhor.",
+  description: "Transformando vidas, construindo um futuro melhor.",
   url: "/static/assets/RelatorioSemestral2023_01.pdf",
   image: "https://www.palavrasdepaz.org/static/images/livro.jpg",
 };
