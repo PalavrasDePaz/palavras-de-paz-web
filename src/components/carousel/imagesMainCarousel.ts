d0a3acc74e5df55a7fe0839ab601786a4fe9d880
JVSRamos <joaovsramosj@gmail.com>
2023-09-15 21:46:32 -0300

    Merge pull request #101 from PalavrasDePaz/develop

--- src/components/carousel/imagesMainCarousel.ts
+++ src/components/carousel/imagesMainCarousel.ts
@@ -1,8 +1,6 @@
-import img01 from "../../../public/static/images/img01_moradores.jpeg";
+import img01 from "../../../public/static/images/img01_moradores.jpg";
 import img02 from "../../../public/static/images/img02_refugees.jpeg";
 import img03 from "../../../public/static/images/img03_refugees.jpeg";
-/* import img04 from "../../../public/static/images/img04_imigrantes.jpeg";
-import img05 from "../../../public/static/images/img05_imigrantes.jpeg"; */
 import img06 from "../../../public/static/images/img06_inmates.jpeg";
 import img07 from "../../../public/static/images/img07_inmates.jpeg";
 import img08 from "../../../public/static/images/img08_violencia.jpeg";
@@ -13,8 +11,6 @@ const imagesMainCarousel = [
   { id: 1, image: img01, text: "Projeto com Moradores de Rua" },
   { id: 2, image: img02, text: "Projeto com Refugiados" },
   { id: 3, image: img03, text: "Projeto com Refugiados" },
-  /*  { id: 4, image: img04, text: "Projeto com Imigrantes" },
-  { id: 5, image: img05, text: "Projeto com Imigrantes" }, */
   { id: 9, image: img09, text: "Projeto com Imigrantes" },
   { id: 10, image: img10, text: "Projeto com Imigrantes" },
   { id: 6, image: img06, text: "Projeto com População Carcerária" },
