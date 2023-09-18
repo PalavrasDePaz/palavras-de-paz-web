a33513baa4358229d6488e4a05286ad41415d85f
Wellington Marques <well06marques@gmail.com>
2023-09-18 08:54:27 -0300

    estilização botões gestão de relatórios

--- src/templates/areaDeTrabalho/components/BtnGestao.jsx
+++ src/templates/areaDeTrabalho/components/BtnGestao.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const BtnGestao = () => {
+import Styles from "../styles/GestaoDeRelatorios.module.css";
+
+const BtnGestao = ({ text }) => {
   function handleClick() {
     return console.log("enviado");
   }
-  return <button onClick={handleClick}>Baixar</button>;
+  return (
+    <button className={Styles.btnGestao} onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
+
+BtnGestao.propTypes = {
+  text: PropTypes.string.isRequired,
 };
 
 export default BtnGestao;
