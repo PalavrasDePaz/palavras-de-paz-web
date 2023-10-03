0478a73d014a4a8d3460e22d73fdfc247cf3cf10
Wellington Marques <well06marques@gmail.com>
2023-10-03 12:15:31 -0300

    corrige link quando clica em Prew Rawar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -93,7 +93,7 @@ function Footer() {
         <div className="colunas-footer">
           <h6 className="footer-title">O Programa</h6>
           <Link href="https://us02web.zoom.us/j/81046198333 ">Reunião</Link>
-          <Link href="https://www.premrawat.com/pt/" target="_blank">
+          <Link href="https://www.premrawat.com/pt-br/" target="_blank">
             Prew Rawat
           </Link>
           <Link href="https://tprf.org/pt-br/" target="_blank">
