781034335d4ec5eccb3373aa660c04463612b97a
jose-pedro <josepedro.bernardes1@gmail.com>
2023-10-11 16:28:05 -0300

    fix: Corrigindo bug ao reservar um caderno.
    feat: Adicionando estados para gerenciar as reservas em tempo real atualizando os campos.

--- src/templates/areaDeTrabalho/components/AvaliarCadernos.tsx
+++ src/templates/areaDeTrabalho/components/AvaliarCadernos.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 import DownloadImage from "../../../../public/static/images/icons/download.svg";
+import { api } from "../../../api";
+import dateUTCFormat from "../../../helpers/dateUTCFormat";
+import dateUTCGenerate from "../../../helpers/dateUTCGenerate";
 import getNotebooksDownload from "../../../helpers/getNotebookDownload";
+import isNotebookReserved from "../../../helpers/isNotebookReserved";
 import useGetNotebooks from "../../../hooks/useGetNotebooks";
+import { INotebooks } from "../types/interfaces";
 
 import styles from "../styles/AvaliarCadernos.module.css";
 
@@ -13,8 +18,41 @@ type AvaliarCadernosProps = {
 
 const AvaliarCadernos = ({ idvol }: AvaliarCadernosProps) => {
   const { data: notebooks } = useGetNotebooks(idvol);
+  const [notebooksIn, setNotebooksIn] = useState<INotebooks[]>([]);
   const naoReservado = "Não reservado";
   const preencher = "Preencher Formulário";
+
+  const putReservationData = async (notebookId: number) => {
+    const reserveData = { idvol, notebookId };
+    const response = await api.put("/notebooks/reservation", reserveData);
+    return response.data;
+  };
+
+  const handleReservation = async (notebookId: number) => {
+    const updatedNotebooks = notebooksIn.map((notebook) => {
+      if (notebook.notebookId === notebookId) {
+        return {
+          ...notebook,
+          reserved: true,
+        };
+      }
+      return notebook;
+    });
+
+    setNotebooksIn(updatedNotebooks);
+
+    await putReservationData(notebookId);
+  };
+
+  useEffect(() => {
+    if (notebooks) {
+      const updatedNotebooks = notebooks.map((notebook) => ({
+        ...notebook,
+        reserved: isNotebookReserved(notebook.reservationDate),
+      }));
+      setNotebooksIn(updatedNotebooks);
+    }
+  }, [notebooks]);
 
   return (
     <section className={styles.avaliar_section}>
@@ -27,13 +65,22 @@ const AvaliarCadernos = ({ idvol }: AvaliarCadernosProps) => {
           <h2>Baixar Caderno</h2>
           <h2>Formulário de avaliação</h2>
         </div>
-        {notebooks &&
-          notebooks.map(
-            ({ notebookId, studentName, reservationDate, studentId }) => (
+        {notebooksIn &&
+          notebooksIn.map(
+            ({
+              notebookId,
+              studentName,
+              reservationDate,
+              studentId,
+              reserved,
+            }) => (
               <div key={studentId} className={styles.avaliar_status}>
-                {reservationDate === null ? (
+                {!reserved ? (
                   <>
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      onChange={() => handleReservation(notebookId)}
+                    />
                     <p className={styles.avaliar_status_p1}>{studentName}</p>
                     <p>{naoReservado}</p>
                     <div className={styles.avaliar_status_div}>
@@ -45,7 +92,11 @@ const AvaliarCadernos = ({ idvol }: AvaliarCadernosProps) => {
                   <>
                     <input type="checkbox" checked />
                     <p className={styles.avaliar_status_p1}>{studentName}</p>
-                    <p>{reservationDate}</p>
+                    <p>
+                      {reservationDate
+                        ? dateUTCFormat(reservationDate)
+                        : dateUTCGenerate()}
+                    </p>
                     <div className={styles.avaliar_status_div}>
                       <button
                         onClick={() =>
@@ -62,7 +113,7 @@ const AvaliarCadernos = ({ idvol }: AvaliarCadernosProps) => {
                 )}
                 <p
                   className={`${styles.avaliar_status_p5}
-                ${reservationDate ? styles.avaliar_status_p5_active : ""}`}
+                ${reserved ? styles.avaliar_status_p5_active : ""}`}
                 >
                   {preencher}
                 </p>
