1e61a9e73e635da64635c9bc7c99ae716178e8e8
jose-pedro <josepedro.bernardes1@gmail.com>
2023-10-26 20:00:05 -0300

    Definindo função com zustand para armazenar um token em um estado global.

---
+++ src/zustand/useTokenStore.ts
@@ -0,0 +1,29 @@
+// useTokenStore.ts
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+const LOCAL_STORAGE_KEY = "authToken";
+
+// Defina uma função para obter o token do Local Storage no início.
+const getInitialToken = () => localStorage.getItem(LOCAL_STORAGE_KEY) || "";
+
+export const useTokenStore = create(
+  persist(
+    (set, get) => ({
+      token: getInitialToken(),
+
+      setToken: (newToken: string) => {
+        set({ token: newToken });
+        localStorage.setItem(LOCAL_STORAGE_KEY, newToken);
+      },
+
+      clearToken: () => {
+        set({ token: "" });
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+      },
+    }),
+    {
+      name: "token-storage",
+    }
+  )
+);
