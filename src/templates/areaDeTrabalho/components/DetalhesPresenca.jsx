1ee9b376b792d8dde063ccd8b249b0a9414fa322
dumoresco <eduardomorescoiost@gmail.com>
2023-10-31 18:38:58 -0300

    Adicionado requisição para baixar planilha

--- src/templates/areaDeTrabalho/components/DetalhesPresenca.jsx
+++ src/templates/areaDeTrabalho/components/DetalhesPresenca.jsx
@@ -7,6 +7,7 @@ import DatePicker, { registerLocale } from "react-datepicker";
 
 import calendarIcon from "../../../../public/static/images/icons/calendar.svg";
 import IconeLoading from "../../../../public/static/images/icons/iconeLoading.svg";
+import { api } from "../../../api";
 
 import BtnDados from "./BtnDados";
 
@@ -14,8 +15,28 @@ import "react-datepicker/dist/react-datepicker.css";
 import Styles from "../styles/Dados.module.css";
 
 registerLocale("ptBR", ptBR);
+
 export default function DetalhesPresenca() {
   const [selectedDate, setSelectDate] = useState(new Date());
+  const getAttendances = async () => {
+    const selectedDateString = format(selectedDate, "yyyy-MM-dd");
+    const response = await api.get(`/attendances/from/${selectedDateString}`, {
+      responseType: "arraybuffer",
+    });
+
+    const blob = new Blob([response.data], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `presenças-${selectedDateString}.xlsx`; // Nome do arquivo
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+  };
 
   return (
     <section className={Styles.containerSectionDados}>
@@ -50,7 +71,7 @@ export default function DetalhesPresenca() {
       <div className={Styles.dadosSecondDiv}>
         <a href="./">Visualizar na web</a>
         <span> ou </span>
-        <BtnDados />
+        <BtnDados onClick={getAttendances} />
       </div>
     </section>
   );
