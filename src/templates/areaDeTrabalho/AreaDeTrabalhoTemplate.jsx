219022eadc61291306cacfaf73e8cdab663e76db
Wellington Marques <well06marques@gmail.com>
2023-11-08 21:21:58 -0300

    adiciona o CSS no formulário de horas

--- src/templates/areaDeTrabalho/AreaDeTrabalhoTemplate.jsx
+++ src/templates/areaDeTrabalho/AreaDeTrabalhoTemplate.jsx
@@ -8,6 +8,7 @@ import AvaliarRedacoes from "./components/AvaliarRedacoes";
 import DadosPresenca from "./components/DadosPresenca";
 import DetalhesCadastro from "./components/DetalhesCadastro";
 import DetalhesPresenca from "./components/DetalhesPresenca";
+import FormsLevantamentoHoras from "./components/FormsLevantamentoHoras";
 import HeaderAreaDeTrabalho from "./components/HeaderAreaDeTrabalho";
 import PrimeiroBox from "./components/PrimeiroBox";
 import WorkshopsAssistidos from "./components/WorkshopsAssistidos";
@@ -79,6 +80,7 @@ export default function AreaDeTrabalhoTemplate() {
           theme="colored"
         />
       </div>
+      <FormsLevantamentoHoras />
     </>
   );
 }
