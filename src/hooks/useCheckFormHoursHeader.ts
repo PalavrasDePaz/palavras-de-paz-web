aaaec167d7bbf16b92b053c9b8c807d77b99da33
BrunoSouza <desouza88@hotmail.com>
2023-12-14 15:45:22 -0300

    feat: botão deve habilitar nos primeiros 5 dias e caso vol não tenha preenchido o formulário

---
+++ src/hooks/useCheckFormHoursHeader.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+import { api } from "../api/index";
+
+const HTTP_OK = 200;
+
+export const useRequestStatus = (idVol: number) => {
+  const [isRequestSuccessful, setIsRequestSuccessful] = useState(false);
+
+  useEffect(() => {
+    api
+      .head(`/volunteers/hours/${idVol}`)
+      .then((response) => {
+        if (response.status === HTTP_OK) {
+          setIsRequestSuccessful(true);
+        }
+      })
+      .catch((error) => {
+        // console.error(error);
+      });
+  }, [idVol]);
+
+  return isRequestSuccessful;
+};
