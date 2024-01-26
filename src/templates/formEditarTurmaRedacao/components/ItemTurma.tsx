e8f7f7fb6ac2648201bf5543787b103db57c0e49
guilhermegattimarinho <ggattimarinho@gmail.com>
2024-01-26 18:33:43 -0300

    feat: remove o componente CampoEditavel mantendo a lógica concentrada no componente ItemTurma

--- src/templates/formEditarTurmaRedacao/components/ItemTurma.tsx
+++ src/templates/formEditarTurmaRedacao/components/ItemTurma.tsx
@@ -8,19 +8,47 @@ import style from "../styles/ItemTruma.module.css";
 interface ItemTurmaProps {
   label: string;
   value: string;
+  isEditing?: boolean;
   onEditClick: () => void;
+  handleSaveEdit: () => void;
+  handleCancelEdit: () => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const ItemTurma: React.FC<ItemTurmaProps> = ({ label, value, onEditClick }) => (
-  <p className={style.chave}>
-    {label}: <span className={style.valor}>{value}</span>
-    <button type="button" onClick={onEditClick} className={style.editBtn}>
-      <Image
-        src={editBtn}
-        alt="imagem de um lápis, como se fosse para editar"
-      />
-    </button>
-  </p>
+const ItemTurma: React.FC<ItemTurmaProps> = ({
+  label,
+  value,
+  onEditClick,
+  handleSaveEdit,
+  handleCancelEdit,
+  onChange,
+  isEditing = false,
+}) => (
+  <div className={style.ItemTurmaContainer}>
+    <p className={style.chave}>
+      {label}: <span className={style.valor}>{value}</span>
+      <button type="button" onClick={onEditClick} className={style.editBtn}>
+        <Image
+          src={editBtn}
+          alt="imagem de um lápis, como se fosse para editar"
+        />
+      </button>
+    </p>
+
+    {isEditing && (
+      <div>
+        <input
+          className={style.isEditing}
+          type="text"
+          placeholder={`Digite o novo valor para ${label}`}
+          value={value}
+          onChange={onChange}
+        />
+        <button onClick={handleSaveEdit}>✓</button>
+        <button onClick={handleCancelEdit}>x</button>
+      </div>
+    )}
+  </div>
 );
 
 export default ItemTurma;
