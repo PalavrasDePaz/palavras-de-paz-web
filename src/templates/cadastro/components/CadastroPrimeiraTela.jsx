1da908631c877fe843157fc71bf8ce99333999f8
Mariana Correa <mpcs270391@gmail.com>
2024-02-20 14:39:06 -0300

    refactor: Divide texto de cadastro em múltiplas tags p. Altera botões para mandar para página de cadastro em login e voluntários

--- src/templates/cadastro/components/CadastroPrimeiraTela.jsx
+++ src/templates/cadastro/components/CadastroPrimeiraTela.jsx
@@ -46,13 +46,16 @@ export default function cadastroPrimeiraTela({ buttonCallback, data } = props) {
       <section>
         <h1 className={styles.formTitle}>CADASTRO DE VOLUNTÁRIOS</h1>
 
+        <p className={styles.formParagraph}>Seja bem-vindo(a)!</p>
         <p className={styles.formParagraph}>
-          Seja bem-vindo(a)! Preparamos este formulário para podermos te
-          conhecer melhor, saber sobre seus conhecimentos e experiências e para
-          entendermos a sua disponibilidade com o nosso voluntariado. Quanto
-          mais você puder compartilhar com a gente, mais conseguiremos te ajudar
-          a alcançar seus objetivos! O preenchimento tomará no máximo dez
-          minutos do seu tempo! Bora lá?
+          Preparamos este formulário para podermos te conhecer melhor, saber
+          sobre seus conhecimentos e experiências e para entendermos a sua
+          disponibilidade com o nosso voluntariado. Quanto mais você puder
+          compartilhar com a gente, mais conseguiremos te ajudar a alcançar seus
+          objetivos!
+        </p>
+        <p className={styles.formParagraph}>
+          O preenchimento tomará no máximo dez minutos do seu tempo! Bora lá?
         </p>
 
         <section className={styles.cadastroFormSectionInputContainer}>
