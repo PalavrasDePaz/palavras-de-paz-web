61a668aabfd338ad0265da319fbdf5a4d92f8338
André Luiz <andre.luizfr2@gmail.com>
2024-03-25 22:27:59 -0300

    style: melhorias na estilização da página de publicações no mobile.

--- src/templates/publicacoes/links.ts
+++ src/templates/publicacoes/links.ts
@@ -4,26 +4,36 @@ import img03 from "../../../public/static/images/linkPreviews/link03.png";
 
 // Imagens dos previews geradas em https://link-previews.stephanbogner.de/
 
+/* eslint-disable max-len */
 const LINKS = [
   {
     id: 1,
-    href: "/publicacoes/arquivos/o-impacto-de-ouca-a-sua-voz-nas-prisoes-brasileiras",
-    img: img03,
+    href: "https://tprf.org/pt-br/estudo-do-programa-de-educacao-para-a-paz-mostra-beneficios-excepcionais-para-pessoas-encarceradas/",
+    img: img02,
+    host: "TPRF.org",
+    title:
+      "Estudo do Programa de Educação para a Paz Mostra Benefícios Excepcionais para Pessoas Encarceradas -...",
+    intro:
+      "Um novo estudo do Programa de Educação para a Paz revelou benefícios excepcionais para os indivíduos encarcerados, incluindo redução da ansiedade e melhoria da esperança.",
   },
   {
     id: 2,
     href: "https://tprf.org/pt-br/funap-amplia-o-programa-de-educacao-para-a-paz-nas-prisoes-brasileiras/",
-    title:
-      "FUNAP Amplia o Programa de Educação para a Paz nas Prisões Brasileiras",
     img: img01,
+    host: "TPRF.org",
+    title:
+      "FUNAP Amplia o Programa de Educação para a paz nas Prisões Brasileiras - TPRF.org",
+    intro:
+      "A Fundação de Amaparo ao Trabalhador Preso (FUNAP) estabeleceu parceria com a Fundação Prem Rawat para oferecer o Programa de Educação para a Paz em São Paulo, Brasil.",
   },
   {
     id: 3,
-    href: "https://tprf.org/pt-br/estudo-do-programa-de-educacao-para-a-paz-mostra-beneficios-excepcionais-para-pessoas-encarceradas/",
-    title:
-      // eslint-disable-next-line max-len
-      "Estudo do Programa de Educação para a Paz Mostra Benefícios Excepcionais para Pessoas Encarceradas",
-    img: img02,
+    href: "/publicacoes/arquivos/o-impacto-de-ouca-a-sua-voz-nas-prisoes-brasileiras",
+    img: img03,
+    host: "Palavrasdepaz.org",
+    title: 'O impacto de "Ouça a Sua Voz" nas Prisões Brasileiras',
+    intro:
+      "Em 14 de setembro de 2021, a HarperOne publicou o livro revolucionário de Prem Rawat, Ouça a Sua Voz: Como Encontrar a Paz em um Mundo Barulhento. Duas semenas depois, ele se tornou um best-seller do New York Times.",
   },
 ];
 
