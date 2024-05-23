d81e1cbdd6d918bb3e7973454c876186f743e321
Lianny Coringa <lianny_costa@hotmail.com>
2024-05-23 10:27:06 -0300

    Questão 01

--- src/templates/formAvaliarRedacao/components/Question1.tsx
+++ src/templates/formAvaliarRedacao/components/Question1.tsx
@@ -9,38 +9,13 @@ interface Question3AvalProps {
 const Question1: React.FC<Question3AvalProps> = ({ handleChangeQuestions }) => (
   <>
     <h3 className={styles.sectionTitle}>Critérios de não validação</h3>
-    <p>Assinale o campo conforme avaliação.</p>
+    <p className={styles.label}>Assinale o campo conforme avaliação.</p>
     <div>
-      <p>Houve cópia parcial ou integral de outro RL?</p>
-      <div>
-        <label htmlFor="isParcialPlagiarism" className={styles.label}>
-          <input
-            className={styles.radioInput}
-            type="radio"
-            name="isParcialPlagiarism"
-            id="isParcialPlagiarism"
-            value="Sim"
-            onChange={(e) => handleChangeQuestions(e)}
-          />
-          Sim
-        </label>
-        <label htmlFor="isNotParcialPlagiarism" className={styles.label}>
-          <input
-            className={styles.radioInput}
-            type="radio"
-            name="isParcialPlagiarism"
-            id="isNotParcialPlagiarism"
-            value="Não"
-            onChange={(e) => handleChangeQuestions(e)}
-          />
-          Não
-        </label>
-      </div>
-    </div>
-    <div>
-      <p>Houve apropriação indevida do texto?</p>
-      <div>
-        <label htmlFor="isAppropriation" className={styles.label}>
+      <div className={styles.optionContainer}>
+        <p className={styles.radioLabel}>
+          Houve apropriação indevida do texto?
+        </p>
+        <label htmlFor="isAppropriation" className={styles.radioLabel}>
           <input
             className={styles.radioInput}
             type="radio"
@@ -51,12 +26,39 @@ const Question1: React.FC<Question3AvalProps> = ({ handleChangeQuestions }) => (
           />
           Sim
         </label>
-        <label htmlFor="isNotAppropriation" className={styles.label}>
+        <label htmlFor="isNotAppropriation" className={styles.radioLabel}>
           <input
             className={styles.radioInput}
             type="radio"
             name="isAppropriation"
             id="isNotAppropriation"
+            value="Não"
+            onChange={(e) => handleChangeQuestions(e)}
+          />
+          Não
+        </label>
+      </div>
+      <div className={styles.optionContainer}>
+        <p className={styles.radioLabel}>
+          Houve cópia parcial ou integral de outro RL?
+        </p>
+        <label htmlFor="isParcialPlagiarism" className={styles.radioLabel}>
+          <input
+            className={styles.radioInput}
+            type="radio"
+            name="isParcialPlagiarism"
+            id="isParcialPlagiarism"
+            value="Sim"
+            onChange={(e) => handleChangeQuestions(e)}
+          />
+          Sim
+        </label>
+        <label htmlFor="isNotParcialPlagiarism" className={styles.radioLabel}>
+          <input
+            className={styles.radioInput}
+            type="radio"
+            name="isParcialPlagiarism"
+            id="isNotParcialPlagiarism"
             value="Não"
             onChange={(e) => handleChangeQuestions(e)}
           />
