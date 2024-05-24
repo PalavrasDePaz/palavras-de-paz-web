fc6dac3f6de6a539b917c15edf1a344cbde1dbc1
André Luiz <andre.luizfr2@gmail.com>
2024-05-24 19:02:18 -0300

    feat: página para editar turma de redação.

---
+++ src/hooks/useGetBookClassFromId.ts
@@ -0,0 +1,23 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+
+import { api } from "../api";
+import { BookClass } from "../templates/formEditarTurmaRedacao/schema";
+
+const getBookClassFromId = async (bookClassId: string | null) => {
+  if (bookClassId === null) return;
+  const response = await api.get<BookClass[]>(
+    `/book-club-class/from-id/${bookClassId}`
+  );
+  return response.data.find(
+    (bookClass) => bookClass.idclass.toString() === bookClassId
+  );
+};
+
+const useGetBookClassFromId = (
+  bookClassId: string | null
+): UseQueryResult<BookClass> =>
+  useQuery(["bookClass", bookClassId], () => getBookClassFromId(bookClassId), {
+    enabled: !!bookClassId,
+  });
+
+export default useGetBookClassFromId;
