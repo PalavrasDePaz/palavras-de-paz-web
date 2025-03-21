1d828b6073fe876902d85ccf66cf81edff9b9068
DESKTOP-LF74LKS\Andre <andre.luizfr2@gmail.com>
2025-03-21 11:12:03 -0300

    feat: melhorias na tela about

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: "[name].[ext]",
+            outputPath: "video"
+        }
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
