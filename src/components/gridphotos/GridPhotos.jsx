281655ea491254528e9351ed464c31d50a03a90b
DESKTOP-LF74LKS\Andre <andre.luizfr2@gmail.com>
2025-04-19 13:55:07 -0300

    feat: novas fotos na galeria

--- src/components/gridphotos/GridPhotos.jsx
+++ src/components/gridphotos/GridPhotos.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import React from "react";
 import Image from "next/image";
 
@@ -115,6 +116,289 @@ function GridPhotos() {
             src="/static/images/foto11.png"
             width="1320px"
             height="502px"
+          />
+        </div>
+      </container>
+
+      <container className="container4">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto12.jfif"
+            width="816px"
+            height="415px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto13.jfif"
+            width="576px"
+            height="422px"
+          />
+        </div>
+      </container>
+
+      <container className="container5">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto14.JPG"
+            width="1320px"
+            height="572.24px"
+          />
+        </div>
+      </container>
+
+      <container className="container6">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto15.JPG"
+            width="696px"
+            height="517px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto16.png"
+            width="696px"
+            height="517px"
+          />
+        </div>
+      </container>
+
+      <container className="container7">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto17.jpg"
+            width="936px"
+            height="510px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto18.jpg"
+            width="456px"
+            height="510px"
+          />
+        </div>
+      </container>
+
+      <container className="container4">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto19.png"
+            width="576px"
+            height="422px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto20.png"
+            width="816px"
+            height="415px"
+          />
+        </div>
+      </container>
+
+      <container className="container8">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto21.png"
+            width="1320px"
+            height="502px"
+          />
+        </div>
+      </container>
+
+      <container className="container6">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto22.png"
+            width="244px"
+            height="182px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto23.png"
+            width="241px"
+            height="162px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto24.png"
+            width="233px"
+            height="119px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto25.png"
+            width="242px"
+            height="142px"
+          />
+        </div>
+      </container>
+
+      <container className="container8">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto26.jpeg"
+            width="1600px"
+            height="1200px"
+          />
+        </div>
+      </container>
+
+      <container className="container8">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto27.jpeg"
+            width="1280px"
+            height="960px"
+          />
+        </div>
+      </container>
+
+      <container className="container4">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto28.jpeg"
+            width="816px"
+            height="415px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto29.jpeg"
+            width="576px"
+            height="422px"
+          />
+        </div>
+      </container>
+
+      <container className="container5">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto30.jpeg"
+            width="1320px"
+            height="572.24px"
+          />
+        </div>
+      </container>
+
+      <container className="container6">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto31.jpeg"
+            width="696px"
+            height="517px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto32.jpeg"
+            width="696px"
+            height="517px"
+          />
+        </div>
+      </container>
+
+      <container className="container7">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto33.jpeg"
+            width="936px"
+            height="510px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto34.jpeg"
+            width="456px"
+            height="510px"
+          />
+        </div>
+      </container>
+      <container className="container8">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto35.jpeg"
+            width="1280px"
+            height="960px"
+          />
+        </div>
+      </container>
+
+      <container className="container6">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto36.jpeg"
+            width="696px"
+            height="517px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto37.jpeg"
+            width="696px"
+            height="517px"
+          />
+        </div>
+      </container>
+
+      <container className="container7">
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto38.jpeg"
+            width="936px"
+            height="510px"
+          />
+        </div>
+
+        <div>
+          <Image
+            alt=""
+            src="/static/images/foto39.jpeg"
+            width="456px"
+            height="510px"
           />
         </div>
       </container>
