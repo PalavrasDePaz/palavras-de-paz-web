3bf9f9aa6e4cb5d21bc22273c6257370f3797b5a
DESKTOP-LF74LKS\Andre <andre.luizfr2@gmail.com>
2025-05-17 13:45:08 -0300

    fix: 3 vídeos de depoimento na home e remoção do vídeo errado na tela de depoimentos

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,26 +1,28 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import * as S from "./styled";
 
 function Card() {
   return (
     <S.CardContainer>
       <S.CardContent>
-        <iframe
-          title="vimeo-player"
-          src="https://player.vimeo.com/video/1068413699?h=88e8e87e82"
-          width="840"
-          height="560"
-          allowfullscreen
-        />
+        <video controls style={{ width: "100%", maxWidth: 300 }}>
+          <source src="/static/videos/depoimentos1.mp4" type="video/mp4" />
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
       </S.CardContent>
 
       <S.CardContent>
-        <iframe
-          title="vimeo-player"
-          src="https://player.vimeo.com/video/1068416301?h=45da83ff2c"
-          width="840"
-          height="560"
-          allowfullscreen
-        />
+        <video controls style={{ width: "100%", maxWidth: 300 }}>
+          <source src="/static/videos/depoimentos2.mp4" type="video/mp4" />
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
+      </S.CardContent>
+
+      <S.CardContent>
+        <video controls style={{ width: "100%", maxWidth: 300 }}>
+          <source src="/static/videos/depoimentos3.mp4" type="video/mp4" />
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
       </S.CardContent>
     </S.CardContainer>
   );
